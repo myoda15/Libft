d71b0682dde6f5e108763b4e9dde561f70db95cb
myoda15 <manuelmoraisantunes07@gmail.com>
2025-10-20 20:02:18 +0100

    Add files via upload

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,23 +6,23 @@
 /*   By: mande-so <mande-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 12:14:59 by mande-so          #+#    #+#             */
-/*   Updated: 2025/10/20 14:14:45 by mande-so         ###   ########.fr       */
+/*   Updated: 2025/10/20 19:50:27 by mande-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *s, int c, size_t len)
 {
 	size_t	i;
 
-	if (!b)
+	if (!s)
 		return (NULL);
 	i = 0;
 	while (i < len)
 	{
-		((unsigned char *)b)[i] = (unsigned char) c;
+		((unsigned char *)s)[i] = (unsigned char) c;
 		i++;
 	}
-	return (b);
+	return (s);
 }
